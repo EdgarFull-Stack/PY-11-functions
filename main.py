f9e83ea6610fbfb6a956f3299c1bf1b7956e88b2
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-29 19:28:57 +0200

    4 and 5 task add

--- main.py
+++ main.py
@@ -20,3 +20,22 @@ def multiply_all_by_numb(numb, *args):
         print(elem * numb)
 
 multiply_all_by_numb(7, 10, 11, 50)
+# Example
+def multiply_all_by_numb(numb, *args, text="* DAUGYBA"):
+    for elem in args:
+        print(f"{elem * numb} {text}")
+
+multiply_all_by_numb(7, 10, 11, 50)
+multiply_all_by_numb(7, 10, 11, 50, text="***")
+# Example
+def return_list_of_multiplied_numbs(numb, *args, info=False):
+    multiplied_numbs = [elem * numb for elem in args]
+    if info:
+        print(f"daugiklis: {numb}, args: {args}, rezultatas: {multiplied_numbs}")
+    return multiplied_numbs
+
+res = return_list_of_multiplied_numbs(7, 10, 11, 50)
+print(res)  # [70, 77, 350]
+
+res = return_list_of_multiplied_numbs(3, 10, 11, 50, info=True)  # daugiklis yra: 3 *args yra: (10, 11, 50) rezultatas yra: [30, 33, 150]
+print(res)  # [30, 33, 150]
--- tasks.py
+++ tasks.py
@@ -15,3 +15,16 @@ def pakelti_laipsniu(n, *args):
     for num in args:
         print(n**num)
 pakelti_laipsniu(3, 2, 3, 4)
+# 4 Task
+def spausdinti_zinutes(kartai, *args, pabaiga="!"):
+    for zinute in args:
+        print((zinute +" ")*kartai+pabaiga)
+spausdinti_zinutes(3, "Labas", "Kaip sekasi", pabaiga=".")
+# 5 Task
+def dauginti_skaicius(n: int, *args: int):
+     return [elem * n for elem in args]
+print(dauginti_skaicius(7, 10, 11, 50))
+
+
+
+
